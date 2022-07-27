8eb1a6e2cc435f0b00f9fbf520214e0b2f6c71ef
Venkatesh Boopathy <venkatesh.boopathy@twilightsoftwares.com>
2022-07-27 18:37:14 +0530

    feat create a new angular project

---
+++ imercifycode/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'projects';
+  onActivate(_event: any) {
+    // window.scroll(0,0);
+ 
+    window.scroll({ 
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth' 
+     });
+ 
+     
+ }
+}
---
+++ imercifycode/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+import { WhyimrcfyComponent } from './whyimrcfy/whyimrcfy.component';
+import { FeaturesComponent } from './features/features.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LandingComponent,
+    FooterComponent,
+    WhyimrcfyComponent,
+    FeaturesComponent,
+    SolutionsComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxUsefulSwiperModule,
+    NgbModule,
+    // BrowserAnimationsModule
+  ],
+  providers: [
+     
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
